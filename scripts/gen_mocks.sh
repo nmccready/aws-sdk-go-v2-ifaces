f6c493ad52e088a3d6049f6bd368da76b5359477
nmccready <nemtcan@gmail.com>
2024-07-12 20:52:30 -0400

    fix: don't race to commit on generating mocks

--- scripts/gen_mocks.sh
+++ scripts/gen_mocks.sh
@@ -10,32 +10,35 @@ cd "$SCRIPT_DIR"
 cd ../
 
 # Iterate over each local ./service directory
-# generate mocks in parallel in batches of 5
+# generate mocks in parallel in batches of 15
 i=0
 for service_dir in ./service/*; do
     # and not the "internal" directory
     if [ -d "$service_dir"  ] && [ "$(basename "$service_dir")" != "internal" ]; then
-    echo "==============================="
-    echo "Generating IClient mock for $(basename "$service_dir")..."
-    # execute in background bumping up against the limit of 5
-    # capture the process id
-    mockery --dir "$service_dir" --name IClient --output "$service_dir/mocks" > /dev/null 2>&1 &
-    # capture the process id
-    pids[${i}]=$!
-    i=$((i+1))
-    if [ $i -eq 10 ]; then
-        echo "Waiting for mocks to generate..."
-        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        # wait for all 5 to finish
-        for pid in ${pids[*]}; do
-            wait $pid
-        done
-        i=0
-    fi
-    
+        echo "==============================="
+        echo "Generating IClient mock for $(basename "$service_dir")..."
+        # execute in background bumping up against the limit of 5
+        # capture the process id
+        mockery --dir "$service_dir" --name IClient --output "$service_dir/mocks" > /dev/null 2>&1 &
+        # capture the process id
+        pids[${i}]=$!
+        i=$((i+1))
+        if [ $i -eq 15 ]; then
+            echo "Waiting for mocks to generate..."
+            echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            # wait for all 5 to finish
+            for pid in ${pids[*]}; do
+                wait $pid
+            done
+            i=0
+        fi
     fi
 done
 
+# wait for any remaining pids to finish
+for pid in ${pids[*]}; do
+    wait $pid
+done
 echo "Mocks generation complete."
 
 cd "$ORIG_DIR"
