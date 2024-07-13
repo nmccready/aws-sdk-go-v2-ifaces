98bec5d8e1ba396f49e8117e0cac437db91346b6
nmccready <nemtcan@gmail.com>
2024-07-13 05:59:16 -0400

    chore: cloudformation test

---
+++ tests/service/cloudformation/iface_test.go
@@ -0,0 +1,19 @@
+package cloudformation_test
+
+// tests for the s3 service interface for this ../../../service/cloudformation/iface.go
+
+import (
+	// "github.com/nmccready/aws-sdk-go-v2-ifaces/service/cloudformation/mocks"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/nmccready/aws-sdk-go-v2-ifaces/service/cloudformation/cloudformation_iface"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestS3ServiceCanBeMocked(t *testing.T) {
+	var iface cloudformation_iface.IClient
+
+	iface = &cloudformation.Client{}
+	assert.NotNil(t, iface, "not nil")
+}
